00d45bcb38737529a52063d619324329d7633147
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2023-11-10 22:59:56 +0100

    Use nix-direnv for improved flake support
    The nix-direnv implementation prevents the flake inputs from getting
    garbage collected.

--- home/modules/base.nix
+++ home/modules/base.nix
@@ -16,5 +16,17 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs.direnv.enable = true;
+  programs.direnv = {
+    enable = true;
+
+    # The 'use flake' implementation in the direnv standard
+    # library already offers good flake support. But nix-direnv
+    # still has one big advantage. It makes the inputs of the
+    # flake a garbage collection root. Without that feature one
+    # has to download the tarballs of the inputs after each
+    # garbage collection run. See:
+    # https://github.com/nix-community/nix-direnv#flakes-support
+    # https://github.com/nix-community/nix-direnv/blob/master/direnvrc#L282
+    nix-direnv.enable = true;
+  };
 }
