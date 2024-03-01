3b38c49d87938aa7f9bcfc1f488a0d6ce35c4c1e
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2024-03-01 09:47:31 +0100

    Add garbage collection of Home Manager generations

--- home/modules/base.nix
+++ home/modules/base.nix
@@ -6,11 +6,20 @@
 # received a copy of the license along with this program.
 
 {
-  home = {
-    # Declare which Home Manager release the configuration is
-    # compatible with. This avoids accidental introduction of
-    # backwards incompatible changes.
-    stateVersion = "23.11";
+  # Declare which Home Manager release the configuration is
+  # compatible with. This avoids accidental introduction of
+  # backwards incompatible changes.
+  home. stateVersion = "23.11";
+
+  # We also need to run the Nix garbage collector as the
+  # Home Manager user. Because the generations in the
+  # ~/.local/state/nix/profiles directory aren't deleted
+  # by the system level garbage collector.
+  nix.gc = {
+    automatic = true;
+    # Sadly daily is not a supported value right now.
+    frequency = "weekly";
+    options = "--delete-older-than 10d";
   };
 
   # Let Home Manager install and manage itself.
