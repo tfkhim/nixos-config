0ebe2993790b0c7819f0523bbe4c31d21e24b6b6
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2024-03-30 11:48:57 +0100

    Fix some user space virtualisation issues
    This commit allows one to specify the OVMF and swtpm packages to use.
    This makes it possible to use dfferent versions for those components in
    case there is an issue with their current version.
    
    Furthermore, this commit now also manages some swtpm related
    configuration files. Those files contain store paths which need to be
    updated accordingly.

--- home/modules/virtualisation.nix
+++ home/modules/virtualisation.nix
@@ -7,14 +7,10 @@
 
 { config, pkgs, lib, ... }:
 let
-  inherit (lib) types mkOption mkIf;
-
-  # The full variant of the UEFI firmware is used here
-  # because it contains secure boot support which is
-  # required for Windows 11.
-  ovmf = pkgs.OVMFFull.fd;
-
-  nixOvmfDir = "${config.xdg.dataHome}/libvirt/nix-ovmf";
+  inherit (lib) types mkOption mkPackageOption mkIf;
+  cfg = config.virtualisation;
+  dataDir = "${config.xdg.dataHome}/libvirt";
+  nixOvmfDir = "${dataDir}/nix-ovmf";
 in
 {
   options.virtualisation = {
@@ -23,16 +19,20 @@ in
       type = types.bool;
       default = false;
     };
+
+    ovmf.package = mkPackageOption pkgs [ "OVMF" "fd" ] { };
+
+    swtpm.package = mkPackageOption pkgs "swtpm" { };
   };
 
   config = mkIf config.virtualisation.enable {
     home.packages = with pkgs; [
       libvirt
-      ovmf
+      cfg.ovmf.package
       # The tools provided by swtpm must be in the
       # PATH for virt-manager to be able to create
       # the required TPM files for a user session.
-      swtpm
+      cfg.swtpm.package
       virt-manager
       # The Remote-Viewer program provided by the
       # virt-viewer package can share folders using
@@ -51,7 +51,31 @@ in
       nvram = ["${nixOvmfDir}/OVMF_CODE.fd:${nixOvmfDir}/OVMF_VARS.fd"]
     '';
 
-    xdg.dataFile."${nixOvmfDir}/OVMF_CODE.fd".source = "${ovmf}/FV/OVMF_CODE.fd";
-    xdg.dataFile."${nixOvmfDir}/OVMF_VARS.fd".source = "${ovmf}/FV/OVMF_VARS.fd";
+    xdg.dataFile."${nixOvmfDir}/OVMF_CODE.fd".source = "${cfg.ovmf.package}/FV/OVMF_CODE.fd";
+    xdg.dataFile."${nixOvmfDir}/OVMF_VARS.fd".source = "${cfg.ovmf.package}/FV/OVMF_VARS.fd";
+
+    xdg.configFile."swtpm_setup.conf".text =
+      let
+        stateDir = "${dataDir}/swtpm-localca";
+
+        localcaConf = pkgs.writeText "swtpm-localca.conf" ''
+          statedir = ${stateDir}
+          signingkey = ${stateDir}/signkey.pem
+          issuercert = ${stateDir}/issuercert.pem
+          certserial = ${stateDir}/certserial
+        '';
+
+        localcaOptions = pkgs.writeText "swtpm-localca.options" ''
+          --platform-manufacturer NixOS
+          --platform-version ${config.home.stateVersion}
+          --platform-model Linux
+        '';
+      in
+      ''
+        create_certs_tool = ${cfg.swtpm.package}/bin/swtpm_localca
+        create_certs_tool_config = ${localcaConf}
+        create_certs_tool_options = ${localcaOptions}
+        active_pcr_banks = sha256
+      '';
   };
 }
