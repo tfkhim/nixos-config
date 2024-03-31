d1066203746d6b5a4c1664cf26aff6917e5a737e
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2024-03-31 09:09:10 +0200

    Move direnv configuration to the base-desktop module
    The base module should only contain the bare minimum that should be
    present on each and every system. The direnv tool is used for
    development environments. Development normally takes place on a desktop
    system. Therefore, this seems to be a good place for the configuration.

--- home/base-desktop.nix
+++ home/base-desktop.nix
@@ -17,4 +17,18 @@
   # requires the udisks2 system service which is enabled in
   # the base-desktop.nix system module.
   services.udiskie.enable = true;
+
+  programs.direnv = {
+    enable = true;
+
+    # The 'use flake' implementation in the direnv standard
+    # library already offers good flake support. But nix-direnv
+    # still has one big advantage. It makes the inputs of the
+    # flake a garbage collection root. Without that feature one
+    # has to download the tarballs of the inputs after each
+    # garbage collection run. See:
+    # https://github.com/nix-community/nix-direnv#flakes-support
+    # https://github.com/nix-community/nix-direnv/blob/master/direnvrc#L282
+    nix-direnv.enable = true;
+  };
 }
--- home/modules/base.nix
+++ home/modules/base.nix
@@ -61,18 +61,4 @@
         Path.PathChanged = keyFile;
       };
     };
-
-  programs.direnv = {
-    enable = true;
-
-    # The 'use flake' implementation in the direnv standard
-    # library already offers good flake support. But nix-direnv
-    # still has one big advantage. It makes the inputs of the
-    # flake a garbage collection root. Without that feature one
-    # has to download the tarballs of the inputs after each
-    # garbage collection run. See:
-    # https://github.com/nix-community/nix-direnv#flakes-support
-    # https://github.com/nix-community/nix-direnv/blob/master/direnvrc#L282
-    nix-direnv.enable = true;
-  };
 }
