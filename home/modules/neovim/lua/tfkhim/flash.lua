843178b2f7eb12720f1c87bed22b2bc9faac4d41
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2025-05-31 22:01:59 +0200

    Disable the character search integration of flash.nvim

--- home/modules/neovim/lua/tfkhim/flash.lua
+++ home/modules/neovim/lua/tfkhim/flash.lua
@@ -14,9 +14,7 @@ require("flash").setup({
             enabled = false,
         },
         char = {
-            highlight = {
-                backdrop = false,
-            },
+            enabled = false,
         },
     },
 
