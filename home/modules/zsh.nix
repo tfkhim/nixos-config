69344696450735d417a708b9fb0a08727f171954
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2025-06-06 22:05:27 +0200

    Use the same history find setup as oh-my-zsh
    Links:
      [1] https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/key-bindings.zsh

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -46,11 +46,13 @@
         '';
 
         historyPrefixCompletion = ''
-          autoload -Uz history-search-end
-          zle -N history-beginning-search-backward-end history-search-end
-          zle -N history-beginning-search-forward-end history-search-end
-          bindkey "$terminfo[kcuu1]" history-beginning-search-backward-end
-          bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
+          autoload -U up-line-or-beginning-search
+          zle -N up-line-or-beginning-search
+          bindkey "$terminfo[kcuu1]" up-line-or-beginning-search
+
+          autoload -U down-line-or-beginning-search
+          zle -N down-line-or-beginning-search
+          bindkey "$terminfo[kcud1]" down-line-or-beginning-search
         '';
 
         awsProfileSelection = ''
