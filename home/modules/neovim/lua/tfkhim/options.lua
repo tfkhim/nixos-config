8a391cbc288d1b8a6ffad689c0887851d32b10c4
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2025-07-19 13:03:16 +0200

    Let visual indention changes in NeoVim not exit visual mode

--- home/modules/neovim/lua/tfkhim/options.lua
+++ home/modules/neovim/lua/tfkhim/options.lua
@@ -48,3 +48,6 @@ vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>")
 
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
