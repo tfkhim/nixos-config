95e7cc2e20b4d15a78b3b5b84e2c2d7018f76d72
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2025-08-02 10:35:05 +0200

    Allow the file tree to increase its width if needed

--- home/modules/neovim/lua/tfkhim/nvim-tree.lua
+++ home/modules/neovim/lua/tfkhim/nvim-tree.lua
@@ -5,7 +5,14 @@
 -- This software is subject to the MIT license. You should have
 -- received a copy of the license along with this program.
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    view = {
+        width = {
+            min = 30,
+            max = 60,
+        },
+    },
+})
 
 vim.keymap.set("n", "<leader>et", require("nvim-tree.api").tree.toggle, { desc = "[E]xplorer: [T]oggle" })
 
