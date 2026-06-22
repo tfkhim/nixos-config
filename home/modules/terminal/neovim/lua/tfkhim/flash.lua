97b40dd6630649784a2a9c79167d72be62fbc89b
Thomas Himmelstoss <9889638+tfkhim@users.noreply.github.com>
2026-06-22 20:50:51 +0200

    Add a two label word jump function

--- home/modules/terminal/neovim/lua/tfkhim/flash.lua
+++ home/modules/terminal/neovim/lua/tfkhim/flash.lua
@@ -5,7 +5,9 @@
 -- This software is subject to the MIT license. You should have
 -- received a copy of the license along with this program.
 
-require("flash").setup({
+local flash = require("flash")
+
+flash.setup({
     label = {
         uppercase = false,
     },
@@ -22,6 +24,52 @@ require("flash").setup({
         mode = "search",
     },
 })
+
+local function jumpToWord()
+    -- See: https://github.com/folke/flash.nvim?tab=readme-ov-file#-examples
+
+    local function format(opts)
+        return {
+            { opts.match.label1, "FlashMatch" },
+            { opts.match.label2, "FlashLabel" },
+        }
+    end
+
+    flash.jump({
+        search = { mode = "search" },
+        label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+        pattern = [[\<]],
+        action = function(match, state)
+            state:hide()
+            flash.jump({
+                search = { max_length = 0 },
+                highlight = { matches = false },
+                label = { format = format },
+                matcher = function(win)
+                    -- limit matches to the current label
+                    return vim.tbl_filter(function(m)
+                        return m.label == match.label and m.win == win
+                    end, state.results)
+                end,
+                labeler = function(matches)
+                    for _, m in ipairs(matches) do
+                        m.label = m.label2 -- use the second label
+                    end
+                end,
+            })
+        end,
+        labeler = function(matches, state)
+            local labels = state:labels()
+            for m, match in ipairs(matches) do
+                match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+                match.label2 = labels[(m - 1) % #labels + 1]
+                match.label = match.label1
+            end
+        end,
+    })
+end
+
+vim.keymap.set("n", "gw", jumpToWord)
 
 vim.keymap.set("n", "<Leader>jl", function()
     require("flash").jump()
